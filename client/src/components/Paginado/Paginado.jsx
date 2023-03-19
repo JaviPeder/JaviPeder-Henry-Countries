27dbfb4bc50e170f66b5a04e5e0e7ce1c0aa0e8f
JaviPeder~ <javierpedernera@gmail.com>
2023-03-19 13:42:18 -0300

    paginado corto

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./paginado.css"
 
 export default function Paginado({ countriesPerPage, countries, paginado, page }) {
+    const [minPage, setMinPage] = useState(0);
+    const [maxPage, setMaxPage] = useState(5);
+
     const pageNumbers = [];
     for (let i = 0; i < Math.ceil(((countries.length - countriesPerPage) / (countriesPerPage + 1)) + 1); i++) {
         pageNumbers.push(i + 1)
@@ -9,16 +12,30 @@ export default function Paginado({ countriesPerPage, countries, paginado, page }
 
         // ((250-9)/10)+1
     }
-
-
+       const lastPage = Math.ceil(((countries.length - countriesPerPage) / (countriesPerPage + 1)) + 1)
+    let pageGroup= pageNumbers.slice(minPage,maxPage)
+    function nextGroup() {
+        setMinPage(minPage+5);
+        setMaxPage(maxPage+5)
+      }
+      function prevGroup() {
+        if(minPage)
+        setMinPage(minPage-5);
+        setMaxPage(maxPage-5)
+      }
+console.log(pageGroup)
+console.log(lastPage)
+console.log(maxPage)
     return (
         <nav>
-            <ul className="paginado">
-                {typeof countries !== 'string' ? pageNumbers.map(number => (
+            <ul className="paginado" >
+                <p className={minPage === 0? "inactive" : null} onClick= { minPage === 0? null :()=> prevGroup()} >...</p>
+                {typeof countries !== 'string' ? pageGroup.map(number => (
                     <li className={page === number ? 'pageselect' : null} key={number}>
                         <p onClick={() => paginado(number)}>{number}</p>
                     </li>
                 )) : <div> <p>1</p> </div>}
+                <p className={maxPage >= lastPage? "inactive" : null} onClick= {maxPage >= lastPage? null :()=> nextGroup()}>...</p>
             </ul>
         </nav>
     )
